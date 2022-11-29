eb6d6bab8f315db1092fc1eb3391572c7c5519ad
Wondo Indra <wondoindra28@gmail.com>
2022-11-30 00:42:06 +0700

    fix sequelize pathing, add validation to signup

--- models/index.js
+++ models/index.js
@@ -22,7 +22,8 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    console.log(path.join(__dirname, file))
+    const model = require(path.join(__dirname, file))
     db[model.name] = model;
   });
 
