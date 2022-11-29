fed7b8f5026d6c5d1760ecb2d3ca8d5e286883ce
Wondo Indra <wondoindra28@gmail.com>
2022-11-30 00:55:52 +0700

    remove log

--- models/index.js
+++ models/index.js
@@ -22,7 +22,6 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    console.log(path.join(__dirname, file))
     const model = require(path.join(__dirname, file))
     db[model.name] = model;
   });
