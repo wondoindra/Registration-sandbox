57191f26b040d63287ec70609cb570eac3cc9fc4
Wondo Indra <wondoindra28@gmail.com>
2022-11-30 19:16:13 +0700

    add aftercreate hook

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize')
+const sendmail = require('../mailer.js')
 
 const { sequelize } = require('./index')
 
@@ -7,6 +8,9 @@ User.init({
   email: DataTypes.STRING,
   password: DataTypes.STRING,
 }, {
+  hooks: {
+    afterCreate: (user) => sendmail(user),
+  },
   sequelize,
   modelName: 'User',
 })
